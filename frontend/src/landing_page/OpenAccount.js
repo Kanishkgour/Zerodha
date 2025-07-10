06dcc96c6360bdbd3ede84d066954692ece9d2de
Kanishk Gour <kanishkgour0@gmail.com>
2025-07-10 18:13:01 +0530

    Update OpenAccount.js

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -6,7 +6,7 @@ function OpenAccount() {
             <div className=' container text-center mt-5'>
                 <h1 className='mt-3 open-acc-heading'>Open a Zerodha account</h1>
                 <p className=' text-muted mt-3 fs-4'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button className=' p-2  mx-auto mt-3 signup-Btn '>Sign up for free</button>
+                <button className=' p-2  mx-auto mt-3 signup-Btn ' > <a href='/signup' className=' text-decoration-none text-white'>Sign up for free</a> </button>
 
             </div>
         </>
